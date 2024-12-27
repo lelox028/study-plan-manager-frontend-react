71aeb9434ed2b99d3d2f0d699d9722acd85ac245
Lelox028 <giulianopascarelli@gmail.com>
2024-12-27 19:58:24 -0300

    basic table example added

--- my-app/src/Carrera.js
+++ my-app/src/Carrera.js
@@ -3,6 +3,14 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Container } from '@mui/material';
 import axios from 'axios';
+// import stuff for tables
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 function Carrera() {
@@ -22,6 +30,19 @@ function Carrera() {
             });
     }, [])
 
+    //table logic
+    function createData(name, calories, fat, carbs, protein) {
+        return { name, calories, fat, carbs, protein };
+    }
+
+    const rows = [
+        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+        createData('Eclair', 262, 16.0, 24, 6.0),
+        createData('Cupcake', 305, 3.7, 67, 4.3),
+        createData('Gingerbread', 356, 16.0, 49, 3.9),
+    ];
+
     return (
         <div className={styles.Body}>
             <div className={styles.TopBar}>
@@ -35,7 +56,38 @@ function Carrera() {
                     <div className={styles.MainHeader}>
                         <h2>{thisCarrera.nombreC}</h2>
                     </div>
-                    <div className={styles.Table}>aa</div>
+                    <div className={styles.Table}>
+                        {/* here goes a table */}
+                        <TableContainer component={Paper}>
+                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Dessert (100g serving)</TableCell>
+                                        <TableCell align="right">Calories</TableCell>
+                                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {rows.map((row) => (
+                                        <TableRow
+                                            key={row.name}
+                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell component="th" scope="row">
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell align="right">{row.calories}</TableCell>
+                                            <TableCell align="right">{row.fat}</TableCell>
+                                            <TableCell align="right">{row.carbs}</TableCell>
+                                            <TableCell align="right">{row.protein}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div>
                 </div>
             </Container>
         </div>
