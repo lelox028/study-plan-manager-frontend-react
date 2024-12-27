556a4432f4b6c1d7022d5761c06caf8676fbb01e
Lelox028 <giulianopascarelli@gmail.com>
2024-12-27 19:28:16 -0300

    Update Carrera.js

--- my-app/src/Carrera.js
+++ my-app/src/Carrera.js
@@ -1,14 +1,40 @@
 import './dist/Carrera.scss';
+import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Container } from '@mui/material';
+import axios from 'axios';
 
-function Carrera (){
+
+function Carrera() {
 
     const { slug } = useParams();
-    console.log("this is an slug",slug)
 
-    return(
-        <div>
-            <h1>CARRERA</h1>
+    const [thisCarrera, setThisCarrera] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get('http://localhost:8080/carreras/' + slug)
+            .then(response => {
+                console.log("Datos carrera recibidos: ", response.data);
+                setThisCarrera(response.data);
+            })
+            .catch(error => {
+                console.error('Error al obtener la carrera:', error);
+            });
+    }, [])
+
+    return (
+        <div className='Body'>
+            <div className='TopBar'>
+                <Container className='TopBarContainer'>
+                    <div className='Left'>left</div>
+                    <div className='Right'>right</div>
+                </Container>
+            </div>
+            <Container maxWidth='lg'>
+                <div className='Main'>
+                    <div className='Table'></div>
+                </div>
+            </Container>
         </div>
     )
 }
