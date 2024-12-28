625dac09702145224c470dfa4ad2e093ac38afc9
Frank <franco.arteche.2b@gmail.com>
2024-12-28 17:05:54 -0300

    Changing the Table to Div to create Component

--- my-app/src/Carrera.js
+++ my-app/src/Carrera.js
@@ -1,132 +1,114 @@
-import styles from './dist/Carrera.module.scss';
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { Container } from '@mui/material';
-import axios from 'axios';
-// import stuff for tables
-import {
-    TableContainer,
-    Table,
-    TableHead,
-    TableRow,
-    TableCell,
-    TableBody,
-    Paper,
-    Typography,
-  } from '@mui/material';
-
+import styles from "./dist/carrera.module.scss";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Container } from "@mui/material";
+import axios from "axios";
+import { Popover } from "@mui/material";
 
 function Carrera() {
+  const { slug } = useParams();
 
-    const { slug } = useParams();
+  const [thisCarrera, setThisCarrera] = React.useState([]);
+  const [materias, setMaterias] = React.useState([]);
 
-    const [thisCarrera, setThisCarrera] = React.useState([]);
-    const [materias, setMaterias] = React.useState([]);
+  React.useEffect(() => {
+    // get Carrera Data
+    axios
+      .get("http://localhost:8080/carreras/" + slug)
+      .then((response) => {
+        console.log("Datos carrera recibidos: ", response.data);
+        setThisCarrera(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener la carrera:", error);
+      });
+    // get all materias for this carrera
+    axios
+      .get("http://localhost:8080/materias/carreras/" + slug + "/materias")
+      .then((response) => {
+        console.log("Datos materias recibidos: ", response.data);
+        setMaterias(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener las materias:", error);
+      });
+  }, []);
 
-    React.useEffect(() => {
-        // get Carrera Data
-        axios.get('http://localhost:8080/carreras/' + slug)
-            .then(response => {
-                console.log("Datos carrera recibidos: ", response.data);
-                setThisCarrera(response.data);
-            })
-            .catch(error => {
-                console.error('Error al obtener la carrera:', error);
-            });
-        // get all materias for this carrera
-        axios.get('http://localhost:8080/materias/carreras/' + slug + '/materias')
-            .then(response => {
-                console.log("Datos materias recibidos: ", response.data);
-                setMaterias(response.data);
-            })
-            .catch(error => {
-                console.error('Error al obtener las materias:', error);
-            });
-    }, [])
-
-
-
-    //table logic
-    function createData(name, calories, fat, carbs, protein) {
-        return { name, calories, fat, carbs, protein };
-    }
-
-    const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-    ];
-
-    return (
-        <div className={styles.Body}>
-            <div className={styles.TopBar}>
-                <Container className={styles.TopBarContainer}>
-                    <div className={styles.Left}>left</div>
-                    <div className={styles.Right}>right</div>
-                </Container>
-            </div>
-            <Container maxWidth='lg'>
-                <div className={styles.Main}>
-                    <div className={styles.MainHeader}>
-                        <h2>{thisCarrera.nombreC}</h2>
-                    </div>
-                    <div className={styles.Table}>
-                        {/* here goes a table */}
-                        <TableContainer component={Paper}>
-                            <Table sx={{ minWidth: 650 }} aria-label="materias table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Materia</TableCell>
-                                        <TableCell align="right">Año</TableCell>
-                                        <TableCell align="right">Cuatrimestre</TableCell>
-                                        <TableCell align="right">Estado</TableCell>
-                                        <TableCell align="right">Fecha de Aprobación</TableCell>
-                                        <TableCell align="right">Calificación</TableCell>
-                                        <TableCell align="right">Carrera</TableCell>
-                                        <TableCell align="right">Correlativas</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {materias.map((materia) => (
-                                        <TableRow key={materia.idMateria}>
-                                            <TableCell component="th" scope="row">
-                                                {materia.nombreMateria}
-                                            </TableCell>
-                                            <TableCell align="right">{materia.anio}</TableCell>
-                                            <TableCell align="right">{materia.cuatrimestre}</TableCell>
-                                            <TableCell align="right">{materia.estado}</TableCell>
-                                            <TableCell align="right">
-                                                {materia.fechaAprobacion ? materia.fechaAprobacion : "N/A"}
-                                            </TableCell>
-                                            <TableCell align="right">
-                                                {materia.calificacion !== null ? materia.calificacion : "N/A"}
-                                            </TableCell>
-                                            <TableCell align="right">
-                                                {materia.carrera.nombreC}
-                                            </TableCell>
-                                            <TableCell align="right">
-                                                {materia.correlativas.length > 0 ? (
-                                                    materia.correlativas.map((corr) => (
-                                                        <Typography key={corr.idMateria}>
-                                                            {corr.nombreMateria}
-                                                        </Typography>
-                                                    ))
-                                                ) : (
-                                                    "Ninguna"
-                                                )}
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </div>
+  return (
+    <div className={styles.Body}>
+      <div className={styles.TopBar}>
+        <Container className={styles.TopBarContainer}>
+          <div className={styles.Left}>left</div>
+          <div className={styles.Right}>right</div>
+        </Container>
+      </div>
+      <Container maxWidth="lg">
+        <div className={styles.Main}>
+          <div className={styles.MainHeader}>
+            <h2>{thisCarrera.nombreC}</h2>
+          </div>
+          <div className={styles.Table}>
+              {/* Encabezados */}
+              <div
+              className={styles.tableHeaders}
+              >
+                <div className={styles.singleHeader}> Materia</div>
+                <div className={styles.singleHeader}>Año</div>
+                <div className={styles.singleHeader}>Cuatrimestre</div>
+                <div className={styles.singleHeader}>Estado</div>
+                <div className={styles.singleHeader}>
+                  Fecha de Aprobación
                 </div>
-            </Container>
+                <div className={styles.singleHeader}>Calificación</div>
+                <div className={styles.singleHeader}>Carrera</div>
+                <div className={styles.singleHeader}>Correlativas</div>
+              </div>
+
+              {/* Filas de datos */}
+              {materias.map((materia) => (
+                <div
+                  key={materia.idMateria}
+                  className={styles.dataRows}
+                >
+                  <div className={styles.singleData}>{materia.nombreMateria}</div>
+                  <div className={styles.singleData}>
+                    {materia.anio}
+                  </div>
+                  <div className={styles.singleData}>
+                    {materia.cuatrimestre}
+                  </div>
+                  <div className={styles.singleData}>
+                    {materia.estado}
+                  </div>
+                  <div className={styles.singleData}>
+                    {materia.fechaAprobacion ? materia.fechaAprobacion : "N/A"}
+                  </div>
+                  <div className={styles.singleData}>
+                    {materia.calificacion !== null
+                      ? materia.calificacion
+                      : "N/A"}
+                  </div>
+                  <div className={styles.singleData}>
+                    {materia.carrera.nombreC}
+                  </div>
+                  <div className={styles.singleData}>
+                    {materia.correlativas.length > 0
+                      ? materia.correlativas
+                          .map((corr) => (
+                            <span key={corr.idMateria}>
+                              {corr.nombreMateria}
+                            </span>
+                          ))
+                          .reduce((prev, curr) => [prev, ", ", curr])
+                      : "Ninguna"}
+                  </div>
+                </div>
+              ))}
+          </div>
         </div>
-    )
+      </Container>
+    </div>
+  );
 }
 
 export default Carrera;
